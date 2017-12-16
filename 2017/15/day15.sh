0ee7b47d9b8a7a842e72f773f64bb9f6255b9e75
Jon McClintock <jon.mcclintock@mixpanel.com>
2017-12-16 04:06:28 +0000

    Cleanup, get working

--- 2017/15/day15.sh
+++ 2017/15/day15.sh
@@ -1,30 +1,28 @@
 #!/bin/bash
 
-startA=699
-startB=124
-rounds=5000000
-seedA=16807
-seedB=48271
-factor=2147483647
-criteriaA=4
-criteriaB=8
-matches=0
+startA=699         # Starting value for generator A
+startB=124         # Starting value for generator B
+rounds=5000000     # Number of rounds to perform
+factorA=16807      # Generator A's factor
+factorB=48271      # Generator B's factor
+limit=2147483647   # Modulo for multiplication
+criteriaA=4        # Generator A results must divide evenly by this
+criteriaB=8        # Generator B results must divide evenly by this
 
 gen() {
 	r=$3
 	while :; do
-		(( r=($1*r) % factor ))
+		(( r=($1*r) % limit ))
 		if (( (r % $2) == 0 )); then return; fi
 	done
 }
 
 a=$startA
 b=$startB
+matches=0
 for i in `seq 0 $rounds`; do
-	gen $seedA $criteriaA $a
-	a=$r
-	gen $seedB $criteriaB $b
-	b=$r
+	gen $factorA $criteriaA $a; a=$r
+	gen $factorB $criteriaB $b; b=$r
 
 	if (( (a & 0xffff) == (b & 0xffff) )); then
 		(( matches=matches+1 ))
