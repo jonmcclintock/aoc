6433c8be71ddb815d52133a2825dabcf78750e27
Jon McClintock <jon.mcclintock@mixpanel.com>
2017-12-16 04:02:32 +0000

    Cleanup, get working

--- 2017/15/day15.sh
+++ 2017/15/day15.sh
@@ -8,23 +8,23 @@ seedB=48271
 factor=2147483647
 criteriaA=4
 criteriaB=8
+matches=0
 
 gen() {
 	r=$3
 	while :; do
 		(( r=($1*r) % factor ))
-		if (( (r % $2) == 0 )); then return $r; fi
+		if (( (r % $2) == 0 )); then return; fi
 	done
 }
 
-matches=0
-a=startA
-b=startB
+a=$startA
+b=$startB
 for i in `seq 0 $rounds`; do
-	generate $seedA $criteriaA $a
-	a=$?
-	generate $seedB $criteriaB $b
-	b=$?
+	gen $seedA $criteriaA $a
+	a=$r
+	gen $seedB $criteriaB $b
+	b=$r
 
 	if (( (a & 0xffff) == (b & 0xffff) )); then
 		(( matches=matches+1 ))
