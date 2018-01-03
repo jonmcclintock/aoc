3c47fae08897bebdec7d8075c5ecd8416adb7b06
Jon McClintock <jammer@weak.org>
2018-01-03 17:20:16 +0700

    Day 24 initial version

---
+++ 2017/24/day24.go
@@ -0,0 +1,153 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+const inputFile = "day24-in.txt"
+
+type component struct {
+	a int
+	b int
+}
+
+type componentList map[int][]component
+
+func loadComponents() componentList {
+	file, err := os.Open(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+
+	comps := componentList{}
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		s := strings.Split(scanner.Text(), "/")
+		a, _ := strconv.Atoi(s[0])
+		b, _ := strconv.Atoi(s[1])
+		c := component{a: a, b: b}
+		if _, ok := comps[a]; ok {
+			comps[a] = append(comps[a], c)
+		} else {
+			comps[a] = []component{c}
+		}
+		if _, ok := comps[b]; ok {
+			comps[b] = append(comps[b], c)
+		} else {
+			comps[b] = []component{c}
+		}
+	}
+
+	return comps
+}
+
+func findMaxStrength(comps componentList, pinCount int, curPath []component) ([]component, int) {
+	used := map[component]bool{}
+	for _, c := range curPath {
+		used[c] = true
+	}
+
+	testPath := make([]component, len(curPath)+1)
+	copy(testPath, curPath)
+
+	maxPath := curPath
+	maxStrength := bridgeStrength(curPath)
+
+	//dumpBridge(curPath, maxStrength)
+
+	for _, c := range comps[pinCount] {
+		if skip, ok := used[c]; ok && skip {
+			continue
+		}
+
+		var testCount int
+		if c.a == pinCount {
+			testCount = c.b
+		} else {
+			testCount = c.a
+		}
+		testPath[len(curPath)] = c
+		path, strength := findMaxStrength(comps, testCount, testPath)
+		if strength > maxStrength {
+			maxStrength = strength
+			maxPath = path
+		}
+	}
+
+	return maxPath, maxStrength
+}
+
+func findMaxLength(comps componentList, pinCount int, curPath []component) ([]component, int) {
+	used := map[component]bool{}
+	for _, c := range curPath {
+		used[c] = true
+	}
+
+	testPath := make([]component, len(curPath)+1)
+	copy(testPath, curPath)
+
+	maxPath := curPath
+	maxStrength := bridgeStrength(curPath)
+	maxLength := len(curPath)
+
+	//dumpBridge(curPath, maxStrength)
+
+	for _, c := range comps[pinCount] {
+		if skip, ok := used[c]; ok && skip {
+			continue
+		}
+
+		var testCount int
+		if c.a == pinCount {
+			testCount = c.b
+		} else {
+			testCount = c.a
+		}
+		testPath[len(curPath)] = c
+		path, strength := findMaxLength(comps, testCount, testPath)
+		if (len(path) > maxLength) || ((len(path) == maxLength) && (strength > maxStrength)) {
+			maxLength = len(path)
+			maxStrength = strength
+			maxPath = path
+		}
+	}
+
+	return maxPath, maxStrength
+}
+
+func bridgeStrength(bridge []component) int {
+	strength := 0
+	for _, c := range bridge {
+		strength += c.a + c.b
+	}
+	return strength
+}
+
+func dumpBridge(bridge []component, strength int) {
+	fmt.Printf("Bridge has strength %d and is %d components long: ", strength, len(bridge))
+	for _, c := range bridge {
+		fmt.Printf("%d/%d, ", c.a, c.b)
+	}
+	fmt.Printf("\n")
+}
+
+func main() {
+	comps := loadComponents()
+
+	fmt.Printf("Finding strongest bridge...\n")
+	bridge, strength := findMaxStrength(comps, 0, []component{})
+	fmt.Printf("Done!\n\n")
+	dumpBridge(bridge, strength)
+
+	fmt.Printf("\nFinding longest bridge...\n")
+	bridge, strength = findMaxLength(comps, 0, []component{})
+	fmt.Printf("Done!\n\n")
+	dumpBridge(bridge, strength)
+}
